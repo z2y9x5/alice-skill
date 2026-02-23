7d28c728eb010f5e9402bff59606ffbd8adfe1bd
Aleksandr Egorov <z2y9x6@ya.ru>
2026-02-23 22:18:51 +0300

    Урок 2
    Добавлены тесты для функции webhook.

---
+++ cmd/skill/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWebhook(t *testing.T) {
+	successBody := `{
+        "response": {
+            "text": "Извините, я пока ничего не умею"
+        },
+        "version": "1.0"
+    }`
+
+	testCases := []struct {
+		method       string
+		expectedCode int
+		expectedBody string
+	}{
+		{method: http.MethodGet, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
+		{method: http.MethodPut, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
+		{method: http.MethodDelete, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
+		{method: http.MethodPost, expectedCode: http.StatusOK, expectedBody: successBody},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.method, func(t *testing.T) {
+			r := httptest.NewRequest(tc.method, "/", nil)
+			w := httptest.NewRecorder()
+
+			webhook(w, r)
+
+			assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
+			if tc.expectedBody != "" {
+				assert.JSONEq(t, tc.expectedBody, w.Body.String(), "Тело ответа не совпадает с ожидаемым")
+			}
+		})
+	}
+}
